84c30ae4c6f63af35c4729a97f27c80c2551afb5
vivekposwal <vivekposwal170782@gmail.com>
2023-03-15 00:59:26 +0530

    build done

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const connectDB = require("./db");
 const pizzaRoutes = require("./routes/pizzasRoute");
 const userRoute = require("./routes/userRoute");
 const ordersRoute = require("./routes/ordersRoute");
-
+const path = require("path");
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Server is working");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is working");
+// });
 app.use("/api/pizzas", pizzaRoutes);
 app.use("/api/users", userRoute);
 app.use("/api/orders", ordersRoute);
+
+//DEPLOYMENT
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //app.get("/getpizzas", (req, res) => {
 //Pizza.find({}, (err, docs) => {
